0a518c68201e5a92ea5ba543b0bbc7b09e5e51c0
bitte <sakisaki8565>
2022-11-24 03:35:58 +0900

    first commit

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ python-mysql-connection-pool-sample
 pythonでコネクションプールを使用するサンプルコード
 
 ## ローカルMysql構築手順
-https://zenn.dev/re24_1986/articles/153cdc5db96dc0
+[【Docker】MySQLを簡単に構築](https://zenn.dev/re24_1986/articles/153cdc5db96dc0)
+[サンプルデータ](https://qiita.com/yukibe/items/fc6016348ecf4f3b10bf)
 ```
 docker-compose up
 mysql -u root -proot -h localhost sample
+SOURCE /Users/ユーザー名/downloads/world.sql;
 ```
--- python-mysql-connection-pool-sample/main.py
+++ python-mysql-connection-pool-sample/main.py
@@ -1,7 +1,9 @@
+from concurrent import futures
 from pprint import pprint
 
 import sqlalchemy
 
+import my_logger
 
 user = 'root'
 password = 'root'
@@ -9,16 +11,10 @@ host = 'localhost'
 port = '3306'
 database = 'sample'
 
+logger = my_logger.get_logger(__name__)
 
-def main():
-    engine = sqlalchemy.create_engine(
-        f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', pool_size=10, max_overflow=10)
-
-    for i in range(20):
-        conn = engine.connect()
-        data = conn.execute('select 1 as test;').fetchall()
-        result = query_to_dict(data)
-        pprint(result)
+engine = sqlalchemy.create_engine(
+    f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', pool_size=10, max_overflow=10)
 
 
 def query_to_dict(data):
@@ -32,6 +28,37 @@ def query_to_dict(data):
         return [{key: value for key, value in row._mapping.items()} for row in data if row is not None]
     else:
         return [{}]
+
+
+def exec_query(sql):
+    """
+    クエリ実行
+    :param sql: SQL
+    :return: 辞書型クエリ結果
+    """
+    with engine.connect() as conn:
+        data = conn.execute(sql).fetchall()
+        result = query_to_dict(data)
+        return result
+
+
+def main_func():
+    try:
+        sql = 'select * from world.city limit 3;'
+        city_list = exec_query(sql)
+        for city in city_list:
+            print(city['Name'])
+    except Exception as e:
+        logger.error(e)
+
+
+def main():
+    future_list = []
+    with futures.ThreadPoolExecutor(max_workers=10) as executor:
+        for i in range(20):
+            future = executor.submit(main_func)
+            future_list.append(future)
+            _ = futures.as_completed(fs=future_list)
 
 
 if __name__ == '__main__':
